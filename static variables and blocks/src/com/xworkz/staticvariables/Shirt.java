30bd1568914247810a9c7685a88b2e87fb65b08c
yashasp25 <yashasp2k3@gmail.com>
2025-03-26 22:45:53 +0530

    Shirt

--- static variables and blocks/src/com/xworkz/staticvariables/Shirt.java
+++ static variables and blocks/src/com/xworkz/staticvariables/Shirt.java
@@ -1,5 +1,13 @@
 package com.xworkz.staticvariables;
 
 public class Shirt {
+    public static int buttons;
 
+    public String color;
+
+
+    static {
+        buttons = 7;
+        System.out.println("Static block called");
+    }
 }
