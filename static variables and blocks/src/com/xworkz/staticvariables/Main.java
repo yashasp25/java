aa0b4987d366dc0f47aede3090ae412935213988
yashasp25 <yashasp2k3@gmail.com>
2025-03-26 22:53:21 +0530

    Main

---
+++ static variables and blocks/src/com/xworkz/staticvariables/Main.java
@@ -0,0 +1,11 @@
+package com.xworkz.staticvariables;
+
+public class Main {
+    public static void main(String[] args) {
+        Shirt shirt = new Shirt();
+        shirt.color = "Blue";
+        System.out.println("Shirt  ...");
+        System.out.println("Buttons : "+Shirt.buttons+ ". Color : "+shirt.color);
+
+    }
+}
