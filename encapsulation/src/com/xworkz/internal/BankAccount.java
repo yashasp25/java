c81f014e041aa8eddf71956093519a27ec5833d8
yashasp25 <yashasp2k3@gmail.com>
2025-03-26 22:16:14 +0530

    Yashas

--- encapsulation/src/com/xworkz/internal/BankAccount.java
+++ encapsulation/src/com/xworkz/internal/BankAccount.java
@@ -3,8 +3,16 @@ package com.xworkz.internal;
 public class BankAccount {
 
     public String name = "Yashas P";
-    Long account_number = 78146164699L;
+    private Long account_number = 78146164699L;
     private String password = "asdf1234";
+
+    public Long getAccount_number(){
+        return account_number;
+    }
+
+    void setAccount_number(Long account_number){
+        this.account_number=account_number;
+    }
 
     String getpassword(){
         return password;
