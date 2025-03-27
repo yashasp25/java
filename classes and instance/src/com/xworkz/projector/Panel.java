6f75a523bd857742cf7ec877f3659296b42ff92a
yashasp25 <yashasp2k3@gmail.com>
2025-03-27 23:00:17 +0530

    Panel

--- classes and instance/src/com/xworkz/projector/Panel.java
+++ classes and instance/src/com/xworkz/projector/Panel.java
@@ -1,4 +1,11 @@
 package com.xworkz.projector;
 
 public class Panel {
+    int numberOfButtons;
+    String material;
+
+    Panel(int numberOfButtons, String material) {
+        this.numberOfButtons = numberOfButtons;
+        this.material = material;
+    }
 }
