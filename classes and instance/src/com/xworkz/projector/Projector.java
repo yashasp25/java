66585c59f6fa4c2951a53202ffe3134439501038
yashasp25 <yashasp2k3@gmail.com>
2025-03-27 23:08:27 +0530

    Projector

--- classes and instance/src/com/xworkz/projector/Projector.java
+++ classes and instance/src/com/xworkz/projector/Projector.java
@@ -1,11 +1,30 @@
 package com.xworkz.projector;
 
 public class Projector {
+    Lens lens;
+    Lamp lamp;
+    Port port;
+    Wire wire;
+    Board board;
+    Button button;
+    Panel panel;
+    Remote remote;
+    Screw screw;
+
+    Projector() {
+        lens = new Lens("Concave", 75);
+        lamp = new Lamp(300, "Osram");
+        port = new Port("USB-C", 3);
+        wire = new Wire("Aluminum", 5);
+        board = new Board("Y456", "Panasonic");
+        button = new Button("Square", "Volume");
+        panel = new Panel(7, "Metal");
+        remote = new Remote("LG", 5000);
+        screw = new Screw("Phillips", 8);
+    }
 
     void startProjector() {
         System.out.println("Start Projection");
-        lamp.turnOn();
-        button.press();
     }
 
     public static void main(String[] args) {
