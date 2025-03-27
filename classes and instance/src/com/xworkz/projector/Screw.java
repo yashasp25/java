996bda61ce7d29badfb3432d663b5e91a63dd916
yashasp25 <yashasp2k3@gmail.com>
2025-03-27 23:05:38 +0530

    Screw

--- classes and instance/src/com/xworkz/projector/Screw.java
+++ classes and instance/src/com/xworkz/projector/Screw.java
@@ -1,4 +1,11 @@
 package com.xworkz.projector;
 
 public class Screw {
+    String type;
+    int size;
+
+    Screw(String type, int size) {
+        this.type = type;
+        this.size = size;
+    }
 }
