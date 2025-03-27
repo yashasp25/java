4f9c9e519e69af31dcd70ac5dbc5cadd647d5471
yashasp25 <yashasp2k3@gmail.com>
2025-03-27 23:05:15 +0530

    Remote

--- classes and instance/src/com/xworkz/projector/Remote.java
+++ classes and instance/src/com/xworkz/projector/Remote.java
@@ -1,4 +1,11 @@
 package com.xworkz.projector;
 
 public class Remote {
+    String brand;
+    int cell;
+
+    Remote(String brand, int batteryCapacity) {
+        this.brand = brand;
+        this.cell = cell;
+    }
 }
