874298cea03177cfaea42f37f5357580c9fab0c3
yashasp25 <yashasp2k3@gmail.com>
2025-03-27 23:01:11 +0530

    Port

--- classes and instance/src/com/xworkz/projector/Port.java
+++ classes and instance/src/com/xworkz/projector/Port.java
@@ -1,4 +1,11 @@
 package com.xworkz.projector;
 
 public class Port {
+    String type;
+    int version;
+
+    Port(String type, int version) {
+        this.type = type;
+        this.version = version;
+    }
 }
