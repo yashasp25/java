b7ab6d4d7a6dd75979efbaccb4a4b48ce699b25b
yashasp25 <yashasp2k3@gmail.com>
2025-03-27 22:59:29 +0530

    Lens

--- classes and instance/src/com/xworkz/projector/Lens.java
+++ classes and instance/src/com/xworkz/projector/Lens.java
@@ -1,4 +1,12 @@
 package com.xworkz.projector;
 
 public class Lens {
+    String type;
+    int focalLength;
+
+    Lens(String type, int focalLength) {
+        this.type = type;
+        this.focalLength = focalLength;
+    }
+
 }
