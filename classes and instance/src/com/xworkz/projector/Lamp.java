a627f384665ec9b9da98e43fa1adaf38e93a36e2
yashasp25 <yashasp2k3@gmail.com>
2025-03-27 22:58:33 +0530

    Lamp

--- classes and instance/src/com/xworkz/projector/Lamp.java
+++ classes and instance/src/com/xworkz/projector/Lamp.java
@@ -1,4 +1,11 @@
 package com.xworkz.projector;
 
 public class Lamp {
+    int wattage;
+    String brand;
+
+    Lamp(int wattage, String brand) {
+        this.wattage = wattage;
+        this.brand = brand;
+    }
 }
