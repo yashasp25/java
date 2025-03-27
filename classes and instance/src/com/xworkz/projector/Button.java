4eb1122d09b94eb488c706337a2355653eeb9183
yashasp25 <yashasp2k3@gmail.com>
2025-03-27 22:56:57 +0530

    Buttons

--- classes and instance/src/com/xworkz/projector/Button.java
+++ classes and instance/src/com/xworkz/projector/Button.java
@@ -1,4 +1,11 @@
 package com.xworkz.projector;
 
 public class Button {
+    String shape;
+    String function;
+
+    Button(String shape, String function) {
+        this.shape = shape;
+        this.function = function;
+    }
 }
