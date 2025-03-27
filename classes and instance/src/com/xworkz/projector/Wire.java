46eedd19b52706fa721bc2294905e4c79bc8420b
yashasp25 <yashasp2k3@gmail.com>
2025-03-27 23:06:33 +0530

    Wire

--- classes and instance/src/com/xworkz/projector/Wire.java
+++ classes and instance/src/com/xworkz/projector/Wire.java
@@ -1,4 +1,11 @@
 package com.xworkz.projector;
 
 public class Wire {
+    String material;
+    int length;
+
+    Wire(String material, int length) {
+        this.material = material;
+        this.length = length;
+    }
 }
