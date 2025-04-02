f3d86193b21579ef5482c2b9d28027058f9cc22e
yashasp25 <yashasp2k3@gmail.com>
2025-04-02 20:24:27 +0530

    Camera

---
+++ inheritance/src/com/xworkz/inheritance/internal/Camera.java
@@ -0,0 +1,8 @@
+package com.xworkz.inheritance.internal;
+
+public class Camera extends Device {
+    public Camera() {
+        super();
+        System.out.println("Constructor in Camera");
+    }
+}
