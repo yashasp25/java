f3c823d23cab2e8f51f6e1c12130c547abf828f5
yashasp25 <yashasp2k3@gmail.com>
2025-04-02 20:42:50 +0530

    Jeans

---
+++ inheritance/src/com/xworkz/inheritance/internal/Jeans.java
@@ -0,0 +1,8 @@
+package com.xworkz.inheritance.internal;
+
+public class Jeans extends Clothing {
+    public Jeans() {
+        super();
+        System.out.println("Constructor in Jeans");
+    }
+}
---
+++ inheritance/src/com/xworkz/inheritance/internal/Shorts.java
@@ -0,0 +1,8 @@
+package com.xworkz.inheritance.internal;
+
+public class Shorts extends Clothing {
+    public Shorts() {
+        super();
+        System.out.println("Constructor in Shorts");
+    }
+}
