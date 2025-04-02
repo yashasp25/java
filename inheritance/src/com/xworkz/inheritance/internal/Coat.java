c0082a73e7b58b96a97439cff6ef9122a3b0e108
yashasp25 <yashasp2k3@gmail.com>
2025-04-02 20:46:21 +0530

    Tshirt

---
+++ inheritance/src/com/xworkz/inheritance/internal/Coat.java
@@ -0,0 +1,8 @@
+package com.xworkz.inheritance.internal;
+
+public class Coat extends Clothing {
+    public Coat() {
+        super();
+        System.out.println("Constructor in Coat");
+    }
+}
---
+++ inheritance/src/com/xworkz/inheritance/internal/TShirt.java
@@ -0,0 +1,8 @@
+package com.xworkz.inheritance.internal;
+
+public class TShirt extends Clothing {
+    public TShirt() {
+        super();
+        System.out.println("Constructor in TShirt");
+    }
+}
