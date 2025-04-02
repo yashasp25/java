4d28ee8a02e2c9bed727b43ff302eb23997cfb7a
yashasp25 <yashasp2k3@gmail.com>
2025-04-02 20:44:20 +0530

    Sweater

---
+++ inheritance/src/com/xworkz/inheritance/internal/Sweater.java
@@ -0,0 +1,8 @@
+package com.xworkz.inheritance.internal;
+
+public class Sweater extends Clothing {
+    public Sweater() {
+        super();
+        System.out.println("Constructor in Sweater");
+    }
+}
