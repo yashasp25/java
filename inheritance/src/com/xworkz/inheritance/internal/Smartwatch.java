55e95fcaa09a0e037590f121f756df600ee90987
yashasp25 <yashasp2k3@gmail.com>
2025-04-02 20:23:32 +0530

    Smartwatch

---
+++ inheritance/src/com/xworkz/inheritance/internal/Smartwatch.java
@@ -0,0 +1,8 @@
+package com.xworkz.inheritance.internal;
+
+public class Smartwatch extends Device {
+    public Smartwatch() {
+        super();
+        System.out.println("Constructor in Smartwatch");
+    }
+}
