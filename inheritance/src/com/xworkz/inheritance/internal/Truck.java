7787f0f56b87d33e12f4c90a4b5854130c14232c
yashasp25 <yashasp2k3@gmail.com>
2025-04-02 20:04:55 +0530

    Truck

---
+++ inheritance/src/com/xworkz/inheritance/internal/Truck.java
@@ -0,0 +1,8 @@
+package com.xworkz.inheritance.internal;
+
+public class Truck extends Vehicle {
+    public Truck() {
+        super();
+        System.out.println("Constructor in Truck");
+    }
+}
