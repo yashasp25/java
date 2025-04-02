536976248eb1ad1794a278dffe5bac544532bae6
yashasp25 <yashasp2k3@gmail.com>
2025-04-02 20:37:38 +0530

    Shirt

---
+++ inheritance/src/com/xworkz/inheritance/internal/Shirt.java
@@ -0,0 +1,8 @@
+package com.xworkz.inheritance.internal;
+
+public class Shirt extends Clothing {
+    public Shirt() {
+        super();
+        System.out.println("Constructor in Shirt");
+    }
+}
