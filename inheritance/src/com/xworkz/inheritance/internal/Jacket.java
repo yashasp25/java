47eacf3adedc7915344f23182ea00586a031e312
yashasp25 <yashasp2k3@gmail.com>
2025-04-02 20:39:48 +0530

    Jsacket

---
+++ inheritance/src/com/xworkz/inheritance/internal/Jacket.java
@@ -0,0 +1,8 @@
+package com.xworkz.inheritance.internal;
+
+public class Jacket extends Clothing {
+    public Jacket() {
+        super();
+        System.out.println("Constructor in Jacket");
+    }
+}
