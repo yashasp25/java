84f917f005f29a5357546e1a8710209c5097ec3b
yashasp25 <yashasp2k3@gmail.com>
2025-04-02 20:20:43 +0530

    smartphone

---
+++ inheritance/src/com/xworkz/inheritance/internal/Smartphone.java
@@ -0,0 +1,8 @@
+package com.xworkz.inheritance.internal;
+
+public class Smartphone extends Device {
+    public Smartphone() {
+        super();
+        System.out.println("Constructor in Smartphone");
+    }
+}
