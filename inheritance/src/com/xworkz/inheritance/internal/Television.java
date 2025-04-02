dbfa4ad2dae89c2fec1588199916a9e7682ff325
yashasp25 <yashasp2k3@gmail.com>
2025-04-02 20:22:38 +0530

    Telivision

---
+++ inheritance/src/com/xworkz/inheritance/internal/Television.java
@@ -0,0 +1,8 @@
+package com.xworkz.inheritance.internal;
+
+public class Television extends Device {
+    public Television() {
+        super();
+        System.out.println("Constructor in Television");
+    }
+}
