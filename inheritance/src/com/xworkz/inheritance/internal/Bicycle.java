f33ce2f07d9afbdad21583a71ce1c1bea4b43787
yashasp25 <yashasp2k3@gmail.com>
2025-04-02 20:07:30 +0530

    Boat

---
+++ inheritance/src/com/xworkz/inheritance/internal/Bicycle.java
@@ -0,0 +1,8 @@
+package com.xworkz.inheritance.internal;
+
+public class Bicycle extends Vehicle {
+    public Bicycle() {
+        super();
+        System.out.println("Constructor in Bicycle");
+    }
+}
---
+++ inheritance/src/com/xworkz/inheritance/internal/Bus.java
@@ -0,0 +1,8 @@
+package com.xworkz.inheritance.internal;
+
+public class Bus extends Vehicle {
+    public Bus() {
+        super();
+        System.out.println("Constructor in Bus");
+    }
+}
