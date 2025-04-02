b7acc1832501f3b4ea43289ddae9ee96059c67f9
yashasp25 <yashasp2k3@gmail.com>
2025-04-02 20:21:38 +0530

    Tablet

---
+++ inheritance/src/com/xworkz/inheritance/internal/Tablet.java
@@ -0,0 +1,8 @@
+package com.xworkz.inheritance.internal;
+
+public class Tablet extends Device {
+    public Tablet() {
+        super();
+        System.out.println("Constructor in Tablet");
+    }
+}
