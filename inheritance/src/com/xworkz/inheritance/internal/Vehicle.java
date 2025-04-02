39f595b29fb7989270d33e343591a281ea278254
yashasp25 <yashasp2k3@gmail.com>
2025-04-02 19:43:14 +0530

    Inheritance

--- inheritance/src/com/xworkz/inheritance/internal/Vehicle.java
+++ inheritance/src/com/xworkz/inheritance/internal/Vehicle.java
@@ -1,4 +1,23 @@
 package com.xworkz.inheritance.internal;
 
 public class Vehicle {
+    void startEngine() {
+        System.out.println("Vehicle: Engine started.");
+    }
+
+    void stopEngine() {
+        System.out.println("Vehicle: Engine stopped.");
+    }
+
+    void accelerate() {
+        System.out.println("Vehicle: Accelerating...");
+    }
+
+    void brake() {
+        System.out.println("Vehicle: Braking...");
+    }
+
+    void honk() {
+        System.out.println("Vehicle: Honking...");
+    }
 }
