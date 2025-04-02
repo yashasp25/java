f25f82c74d063d48a8303911025a5397654f976d
yashasp25 <yashasp2k3@gmail.com>
2025-04-02 20:55:09 +0530

    ClothingRunner

---
+++ inheritance/src/com/xworkz/inheritance/runner/ClothingRunner.java
@@ -0,0 +1,84 @@
+package com.xworkz.inheritance.runner;
+
+import com.xworkz.inheritance.internal.*;
+
+public class ClothingRunner {
+    public static void main(String[] args) {
+        Clothing clothing = new Clothing();
+        clothing.wear();
+        clothing.wash();
+        clothing.dry();
+        clothing.iron();
+        clothing.fold();
+
+        Shirt shirt = new Shirt();
+        shirt.wear();
+        shirt.wash();
+        shirt.dry();
+        shirt.iron();
+        shirt.fold();
+
+        Trousers trousers = new Trousers();
+        trousers.wear();
+        trousers.wash();
+        trousers.dry();
+        trousers.iron();
+        trousers.fold();
+
+        Jacket jacket = new Jacket();
+        jacket.wear();
+        jacket.wash();
+        jacket.dry();
+        jacket.iron();
+        jacket.fold();
+
+        Dress dress = new Dress();
+        dress.wear();
+        dress.wash();
+        dress.dry();
+        dress.iron();
+        dress.fold();
+
+        Skirt skirt = new Skirt();
+        skirt.wear();
+        skirt.wash();
+        skirt.dry();
+        skirt.iron();
+        skirt.fold();
+
+        Jeans jeans = new Jeans();
+        jeans.wear();
+        jeans.wash();
+        jeans.dry();
+        jeans.iron();
+        jeans.fold();
+
+        Shorts shorts = new Shorts();
+        shorts.wear();
+        shorts.wash();
+        shorts.dry();
+        shorts.iron();
+        shorts.fold();
+
+        Sweater sweater = new Sweater();
+        sweater.wear();
+        sweater.wash();
+        sweater.dry();
+        sweater.iron();
+        sweater.fold();
+
+        Coat coat = new Coat();
+        coat.wear();
+        coat.wash();
+        coat.dry();
+        coat.iron();
+        coat.fold();
+
+        TShirt tShirt = new TShirt();
+        tShirt.wear();
+        tShirt.wash();
+        tShirt.dry();
+        tShirt.iron();
+        tShirt.fold();
+    }
+}
