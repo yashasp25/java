84dfc25059321d829ddaf4b42b26799e7294f61d
yashasp25 <yashasp2k3@gmail.com>
2025-04-02 20:25:46 +0530

    Printer

---
+++ inheritance/src/com/xworkz/inheritance/internal/Printer.java
@@ -0,0 +1,8 @@
+package com.xworkz.inheritance.internal;
+
+public class Printer extends Device {
+    public Printer() {
+        super();
+        System.out.println("Constructor in Printer");
+    }
+}
