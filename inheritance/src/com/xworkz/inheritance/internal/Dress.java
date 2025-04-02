dd5815954dbb51010bea3071ce6751601e0cde98
yashasp25 <yashasp2k3@gmail.com>
2025-04-02 20:41:22 +0530

    Skirt

---
+++ inheritance/src/com/xworkz/inheritance/internal/Dress.java
@@ -0,0 +1,8 @@
+package com.xworkz.inheritance.internal;
+
+public class Dress extends Clothing {
+    public Dress() {
+        super();
+        System.out.println("Constructor in Dress");
+    }
+}
---
+++ inheritance/src/com/xworkz/inheritance/internal/Skirt.java
@@ -0,0 +1,8 @@
+package com.xworkz.inheritance.internal;
+
+public class Skirt extends Clothing {
+    public Skirt() {
+        super();
+        System.out.println("Constructor in Skirt");
+    }
+}
