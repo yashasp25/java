42b4e44558b94d9c5ce05442cb5cdf0be99b9bc5
yashasp25 <yashasp2k3@gmail.com>
2025-04-02 20:26:55 +0530

    Speaker

---
+++ inheritance/src/com/xworkz/inheritance/internal/Speaker.java
@@ -0,0 +1,8 @@
+package com.xworkz.inheritance.internal;
+
+public class Speaker extends Device {
+    public Speaker() {
+        super();
+        System.out.println("Constructor in Speaker");
+    }
+}
