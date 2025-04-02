cf96387fb61c23c43793686a3ba3623ceac8573d
yashasp25 <yashasp2k3@gmail.com>
2025-04-02 20:08:36 +0530

    ship

---
+++ inheritance/src/com/xworkz/inheritance/internal/Helicopter.java
@@ -0,0 +1,8 @@
+package com.xworkz.inheritance.internal;
+
+public class Helicopter extends Vehicle {
+    public Helicopter() {
+        super();
+        System.out.println("Constructor in Helicopter");
+    }
+}
---
+++ inheritance/src/com/xworkz/inheritance/internal/Ship.java
@@ -0,0 +1,8 @@
+package com.xworkz.inheritance.internal;
+
+public class Ship extends Vehicle {
+    public Ship() {
+        super();
+        System.out.println("Constructor in Ship");
+    }
+}
