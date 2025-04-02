35bf85678ee58c80a62a02d1ce3ff8d207d0c6bb
yashasp25 <yashasp2k3@gmail.com>
2025-04-02 20:06:17 +0530

    Train

---
+++ inheritance/src/com/xworkz/inheritance/internal/Train.java
@@ -0,0 +1,8 @@
+package com.xworkz.inheritance.internal;
+
+public class Train extends Vehicle {
+    public Train() {
+        super();
+        System.out.println("Constructor in Train");
+    }
+}
