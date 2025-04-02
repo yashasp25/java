15a3e7eb9927a3b99b85805d1ae116175bb0d756
yashasp25 <yashasp2k3@gmail.com>
2025-04-02 20:03:54 +0530

    Airplane

---
+++ inheritance/src/com/xworkz/inheritance/internal/Airplane.java
@@ -0,0 +1,8 @@
+package com.xworkz.inheritance.internal;
+
+public class Airplane extends Vehicle {
+    public Airplane() {
+        super();
+        System.out.println("Constructor in Airplane");
+    }
+}
