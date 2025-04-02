4a273cfeff00c7c7ae245cd4ee987720995fc905
yashasp25 <yashasp2k3@gmail.com>
2025-04-02 20:37:02 +0530

    Clothing

---
+++ inheritance/src/com/xworkz/inheritance/internal/Clothing.java
@@ -0,0 +1,27 @@
+package com.xworkz.inheritance.internal;
+
+public class Clothing {
+    public Clothing() {
+        System.out.println("Constructor in Clothing");
+    }
+
+    public void wear() {
+        System.out.println("Clothing: Wearing...");
+    }
+
+    public void wash() {
+        System.out.println("Clothing: Washing...");
+    }
+
+    public void dry() {
+        System.out.println("Clothing: Drying...");
+    }
+
+    public void iron() {
+        System.out.println("Clothing: Ironing...");
+    }
+
+    public void fold() {
+        System.out.println("Clothing: Folding...");
+    }
+}
