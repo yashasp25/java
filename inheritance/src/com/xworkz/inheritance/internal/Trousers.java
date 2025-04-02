42dd13ca47d11b90608d882fc5a0d4be19f42653
yashasp25 <yashasp2k3@gmail.com>
2025-04-02 20:38:55 +0530

    Trousers

---
+++ inheritance/src/com/xworkz/inheritance/internal/Trousers.java
@@ -0,0 +1,8 @@
+package com.xworkz.inheritance.internal;
+
+public class Trousers extends Clothing {
+    public Trousers() {
+        super();
+        System.out.println("Constructor in Trousers");
+    }
+}
