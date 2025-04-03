4451685c897428c7fa23739c92595c7917c04a7e
yashasp25 <yashasp2k3@gmail.com>
2025-04-03 09:09:20 +0530

    AnimalRunner

---
+++ inheritance/src/com/xworkz/inheritance/runner/AnimalRunner.java
@@ -0,0 +1,76 @@
+package com.xworkz.inheritance.runner;
+
+import com.xworkz.inheritance.internal.*;
+
+public class AnimalRunner {
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        dog.eat();
+        dog.sleep();
+        dog.makeSound();
+        dog.move();
+        dog.breathe();
+
+        Cat cat = new Cat();
+        cat.eat();
+        cat.sleep();
+        cat.makeSound();
+        cat.move();
+        cat.breathe();
+
+        Elephant elephant = new Elephant();
+        elephant.eat();
+        elephant.sleep();
+        elephant.makeSound();
+        elephant.move();
+        elephant.breathe();
+
+        Lion lion = new Lion();
+        lion.eat();
+        lion.sleep();
+        lion.makeSound();
+        lion.move();
+        lion.breathe();
+
+        Tiger tiger = new Tiger();
+        tiger.eat();
+        tiger.sleep();
+        tiger.makeSound();
+        tiger.move();
+        tiger.breathe();
+
+        Horse horse = new Horse();
+        horse.eat();
+        horse.sleep();
+        horse.makeSound();
+        horse.move();
+        horse.breathe();
+
+        Monkey monkey = new Monkey();
+        monkey.eat();
+        monkey.sleep();
+        monkey.makeSound();
+        monkey.move();
+        monkey.breathe();
+
+        Dolphin dolphin = new Dolphin();
+        dolphin.eat();
+        dolphin.sleep();
+        dolphin.makeSound();
+        dolphin.move();
+        dolphin.breathe();
+
+        Eagle eagle = new Eagle();
+        eagle.eat();
+        eagle.sleep();
+        eagle.makeSound();
+        eagle.move();
+        eagle.breathe();
+        Snake snake = new Snake();
+        snake.eat();
+        snake.sleep();
+        snake.makeSound();
+        snake.move();
+        snake.breathe();
+    }
+}
