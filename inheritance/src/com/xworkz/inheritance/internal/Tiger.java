27a73baffca048ce4131aae10be1b6172e8f5ac6
yashasp25 <yashasp2k3@gmail.com>
2025-04-03 09:04:11 +0530

    Tiger

---
+++ inheritance/src/com/xworkz/inheritance/internal/Tiger.java
@@ -0,0 +1,8 @@
+package com.xworkz.inheritance.internal;
+
+public class Tiger extends Animal {
+    public Tiger() {
+        super();
+        System.out.println("Constructor in Tiger");
+    }
+}
