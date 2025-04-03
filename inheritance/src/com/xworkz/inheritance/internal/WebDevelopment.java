095e09eb60893646728da42bf9edd43029dd8ebf
yashasp25 <yashasp2k3@gmail.com>
2025-04-03 09:19:00 +0530

    web

---
+++ inheritance/src/com/xworkz/inheritance/internal/WebDevelopment.java
@@ -0,0 +1,8 @@
+package com.xworkz.inheritance.internal;
+
+public class WebDevelopment extends Subject {
+    public WebDevelopment() {
+        super();
+        System.out.println("Constructor in Web Development");
+    }
+}
