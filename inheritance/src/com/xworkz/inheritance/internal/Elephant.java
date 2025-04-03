58d86699dbe7d043cc10c6cb13a888889523d24e
yashasp25 <yashasp2k3@gmail.com>
2025-04-03 09:02:32 +0530

    Elephant

---
+++ inheritance/src/com/xworkz/inheritance/internal/Elephant.java
@@ -0,0 +1,8 @@
+package com.xworkz.inheritance.internal;
+
+public class Elephant extends Animal {
+    public Elephant() {
+        super();
+        System.out.println("Constructor in Elephant");
+    }
+}
