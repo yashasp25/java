8780231fe323dd5876da9f10ae54111d2ed1981f
yashasp25 <yashasp2k3@gmail.com>
2025-04-03 09:14:18 +0530

    DataStructures

---
+++ inheritance/src/com/xworkz/inheritance/internal/DataStructures.java
@@ -0,0 +1,8 @@
+package com.xworkz.inheritance.internal;
+
+public class DataStructures extends Subject {
+    public DataStructures() {
+        super();
+        System.out.println("Constructor in Data Structures");
+    }
+}
