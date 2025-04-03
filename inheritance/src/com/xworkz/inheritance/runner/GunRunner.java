a81aa39067c7a6f503868fa82c832e93b63a2a91
yashasp25 <yashasp2k3@gmail.com>
2025-04-03 12:27:16 +0530

    GUNRUNNER

---
+++ inheritance/src/com/xworkz/inheritance/runner/GunRunner.java
@@ -0,0 +1,182 @@
+package com.xworkz.inheritance.runner;
+
+import com.xworkz.inheritance.internal.*;
+
+public class GunRunner {
+    public static void main(String[] args) {
+
+        M416 m416 = new M416();
+        m416.shoot();
+        m416.reload();
+        m416.aim();
+        m416.switchMode();
+        m416.inspect();
+
+        AWM awm = new AWM();
+        awm.shoot();
+        awm.reload();
+        awm.aim();
+        awm.switchMode();
+        awm.inspect();
+
+        AKM akm = new AKM();
+        akm.shoot();
+        akm.reload();
+        akm.aim();
+        akm.switchMode();
+        akm.inspect();
+
+        UMP45 ump45 = new UMP45();
+        ump45.shoot();
+        ump45.reload();
+        ump45.aim();
+        ump45.switchMode();
+        ump45.inspect();
+
+        M24 m24 = new M24();
+        m24.shoot();
+        m24.reload();
+        m24.aim();
+        m24.switchMode();
+        m24.inspect();
+
+        SCARL scarl = new SCARL();
+        scarl.shoot();
+        scarl.reload();
+        scarl.aim();
+        scarl.switchMode();
+        scarl.inspect();
+
+        Mini14 mini14 = new Mini14();
+        mini14.shoot();
+        mini14.reload();
+        mini14.aim();
+        mini14.switchMode();
+        mini14.inspect();
+
+        DP28 dp28 = new DP28();
+        dp28.shoot();
+        dp28.reload();
+        dp28.aim();
+        dp28.switchMode();
+        dp28.inspect();
+
+        Kar98k kar98k = new Kar98k();
+        kar98k.shoot();
+        kar98k.reload();
+        kar98k.aim();
+        kar98k.switchMode();
+        kar98k.inspect();
+
+        Groza groza = new Groza();
+        groza.shoot();
+        groza.reload();
+        groza.aim();
+        groza.switchMode();
+        groza.inspect();
+
+        Famas famas = new Famas();
+        famas.shoot();
+        famas.reload();
+        famas.aim();
+        famas.switchMode();
+        famas.inspect();
+
+        QBZ qbz = new QBZ();
+        qbz.shoot();
+        qbz.reload();
+        qbz.aim();
+        qbz.switchMode();
+        qbz.inspect();
+
+        MK14 mk14 = new MK14();
+        mk14.shoot();
+        mk14.reload();
+        mk14.aim();
+        mk14.switchMode();
+        mk14.inspect();
+        MP5K mp5k = new MP5K();
+        mp5k.shoot();
+        mp5k.reload();
+        mp5k.aim();
+        mp5k.switchMode();
+        mp5k.inspect();
+
+        Vector vector = new Vector();
+        vector.shoot();
+        vector.reload();
+        vector.aim();
+        vector.switchMode();
+        vector.inspect();
+
+        S12K s12k = new S12K();
+        s12k.shoot();
+        s12k.reload();
+        s12k.aim();
+        s12k.switchMode();
+        s12k.inspect();
+
+        S1897 s1897 = new S1897();
+        s1897.shoot();
+        s1897.reload();
+        s1897.aim();
+        s1897.switchMode();
+        s1897.inspect();
+
+        S686 s686 = new S686();
+        s686.shoot();
+        s686.reload();
+        s686.aim();
+        s686.switchMode();
+        s686.inspect();
+
+        P90 p90 = new P90();
+        p90.shoot();
+        p90.reload();
+        p90.aim();
+        p90.switchMode();
+        p90.inspect();
+
+        M249 m249 = new M249();
+        m249.shoot();
+        m249.reload();
+        m249.aim();
+        m249.switchMode();
+        m249.inspect();
+
+        AUG aug = new AUG();
+        aug.shoot();
+        aug.reload();
+        aug.aim();
+        aug.switchMode();
+        aug.inspect();
+
+        G36C g36c = new G36C();
+        g36c.shoot();
+        g36c.reload();
+        g36c.aim();
+        g36c.switchMode();
+        g36c.inspect();
+
+        Mk47 mk47 = new Mk47();
+        mk47.shoot();
+        mk47.reload();
+        mk47.aim();
+        mk47.switchMode();
+        mk47.inspect();
+
+        Bizon bizon = new Bizon();
+        bizon.shoot();
+        bizon.reload();
+        bizon.aim();
+        bizon.switchMode();
+        bizon.inspect();
+
+        DBS dbs = new DBS();
+        dbs.shoot();
+        dbs.reload();
+        dbs.aim();
+        dbs.switchMode();
+        dbs.inspect();
+    }
+}
