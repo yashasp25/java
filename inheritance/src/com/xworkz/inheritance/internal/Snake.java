6b2bfef277a3bbef39f9d96ad04f9245bb724fb1
yashasp25 <yashasp2k3@gmail.com>
2025-04-03 09:07:57 +0530

    Snake

---
+++ inheritance/src/com/xworkz/inheritance/internal/Snake.java
@@ -0,0 +1,8 @@
+package com.xworkz.inheritance.internal;
+
+public class Snake extends Animal {
+    public Snake() {
+        super();
+        System.out.println("Constructor in Snake");
+    }
+}
