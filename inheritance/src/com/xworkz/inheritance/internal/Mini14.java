19075eb4dd158c024cc3144e67c7b537ceb1d56a
yashasp25 <yashasp2k3@gmail.com>
2025-04-03 12:08:01 +0530

    mini

---
+++ inheritance/src/com/xworkz/inheritance/internal/Mini14.java
@@ -0,0 +1,8 @@
+package com.xworkz.inheritance.internal;
+
+public class Mini14 extends Gun {
+    public Mini14() {
+        super();
+        System.out.println("Constructor in Mini-14");
+    }
+}
