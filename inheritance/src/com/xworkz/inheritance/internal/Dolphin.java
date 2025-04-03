b734fc660a3b24b6f701141392ff343628af7e98
yashasp25 <yashasp2k3@gmail.com>
2025-04-03 09:05:52 +0530

    Horse

---
+++ inheritance/src/com/xworkz/inheritance/internal/Dolphin.java
@@ -0,0 +1,8 @@
+package com.xworkz.inheritance.internal;
+
+public class Dolphin extends Animal {
+    public Dolphin() {
+        super();
+        System.out.println("Constructor in Dolphin");
+    }
+}
