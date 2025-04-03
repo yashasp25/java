aff01abfdf23a2b9864dce552637f21b79e5fd95
yashasp25 <yashasp2k3@gmail.com>
2025-04-03 09:00:06 +0530

    Animal

---
+++ inheritance/src/com/xworkz/inheritance/internal/Animal.java
@@ -0,0 +1,27 @@
+package com.xworkz.inheritance.internal;
+
+public class Animal {
+    public Animal() {
+        System.out.println("Constructor in Animal");
+    }
+
+    public void eat() {
+        System.out.println("Animal: Eating...");
+    }
+
+    public void sleep() {
+        System.out.println("Animal: Sleeping...");
+    }
+
+    public void makeSound() {
+        System.out.println("Animal: Making sound...");
+    }
+
+    public void move() {
+        System.out.println("Animal: Moving...");
+    }
+
+    public void breathe() {
+        System.out.println("Animal: Breathing...");
+    }
+}
