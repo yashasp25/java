e9b8cda3cef9a02a19b60d95b794a211e77c374f
yashasp25 <yashasp2k3@gmail.com>
2025-04-03 09:07:09 +0530

    Eagle

---
+++ inheritance/src/com/xworkz/inheritance/internal/Eagle.java
@@ -0,0 +1,8 @@
+package com.xworkz.inheritance.internal;
+
+public class Eagle extends Animal {
+    public Eagle() {
+        super();
+        System.out.println("Constructor in Eagle");
+    }
+}
