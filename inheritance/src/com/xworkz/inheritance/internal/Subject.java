98236b1298f438cd628d99bc8c71e50bd778bb2d
yashasp25 <yashasp2k3@gmail.com>
2025-04-03 09:13:27 +0530

    Subject

---
+++ inheritance/src/com/xworkz/inheritance/internal/Subject.java
@@ -0,0 +1,27 @@
+package com.xworkz.inheritance.internal;
+
+public class Subject {
+    public Subject() {
+        System.out.println("Constructor in Subject");
+    }
+
+    public void study() {
+        System.out.println("Subject: Studying...");
+    }
+
+    public void takeExam() {
+        System.out.println("Subject: Taking an exam...");
+    }
+
+    public void submitAssignment() {
+        System.out.println("Subject: Submitting an assignment...");
+    }
+
+    public void discuss() {
+        System.out.println("Subject: Discussing with peers...");
+    }
+
+    public void research() {
+        System.out.println("Subject: Researching new topics...");
+    }
+}
