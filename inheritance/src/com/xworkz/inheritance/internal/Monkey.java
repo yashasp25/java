97ef311da3f3dd726fc41f027f979ab41ba2653d
yashasp25 <yashasp2k3@gmail.com>
2025-04-03 09:06:34 +0530

    Monkey

---
+++ inheritance/src/com/xworkz/inheritance/internal/Monkey.java
@@ -0,0 +1,8 @@
+package com.xworkz.inheritance.internal;
+
+public class Monkey extends Animal {
+    public Monkey() {
+        super();
+        System.out.println("Constructor in Monkey");
+    }
+}
