72cf1db9aa7241fa2652277bd9bafa2c15171516
yashasp25 <yashasp2k3@gmail.com>
2025-04-03 12:31:02 +0530

    Anime

---
+++ inheritance/src/com/xworkz/inheritance/internal/Anime.java
@@ -0,0 +1,27 @@
+package com.xworkz.inheritance.internal;
+
+public class Anime {
+    public Anime() {
+        System.out.println("Constructor in Anime");
+    }
+
+    public void watch() {
+        System.out.println("Watching anime...");
+    }
+
+    public void review() {
+        System.out.println("Reviewing the anime...");
+    }
+
+    public void recommend() {
+        System.out.println("Recommending the anime...");
+    }
+
+    public void discuss() {
+        System.out.println("Discussing anime theories...");
+    }
+
+    public void collectMerchandise() {
+        System.out.println("Collecting anime merchandise...");
+    }
+}
