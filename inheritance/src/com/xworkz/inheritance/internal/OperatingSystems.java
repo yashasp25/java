dc70955be154ec49365cd6f6d98007151db4582f
yashasp25 <yashasp2k3@gmail.com>
2025-04-03 09:16:15 +0530

    Operating systems

---
+++ inheritance/src/com/xworkz/inheritance/internal/OperatingSystems.java
@@ -0,0 +1,8 @@
+package com.xworkz.inheritance.internal;
+
+public class OperatingSystems extends Subject {
+    public OperatingSystems() {
+        super();
+        System.out.println("Constructor in Operating Systems");
+    }
+}
