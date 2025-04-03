cccbd50e0073a336c0e62b14f38580cb4fa281ee
yashasp25 <yashasp2k3@gmail.com>
2025-04-03 09:17:01 +0530

    Computernetworks

---
+++ inheritance/src/com/xworkz/inheritance/internal/ComputerNetworks.java
@@ -0,0 +1,8 @@
+package com.xworkz.inheritance.internal;
+
+public class ComputerNetworks extends Subject {
+    public ComputerNetworks() {
+        super();
+        System.out.println("Constructor in Computer Networks");
+    }
+}
