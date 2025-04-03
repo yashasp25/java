d4b34078b92339cdf35fdf62c4e0592e0bf3e4ed
yashasp25 <yashasp2k3@gmail.com>
2025-04-03 09:20:46 +0530

    ML and CS

---
+++ inheritance/src/com/xworkz/inheritance/internal/CyberSecurity.java
@@ -0,0 +1,8 @@
+package com.xworkz.inheritance.internal;
+
+public class CyberSecurity extends Subject {
+    public CyberSecurity() {
+        super();
+        System.out.println("Constructor in Cyber Security");
+    }
+}
---
+++ inheritance/src/com/xworkz/inheritance/internal/MachineLearning.java
@@ -0,0 +1,8 @@
+package com.xworkz.inheritance.internal;
+
+public class MachineLearning extends Subject {
+    public MachineLearning() {
+        super();
+        System.out.println("Constructor in Machine Learning");
+    }
+}
