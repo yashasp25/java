62b2e2358172f3b2267b08f5e18b600414034a33
yashasp25 <yashasp2k3@gmail.com>
2025-04-05 19:59:49 +0530

    AnimeRunner

--- inheritance/src/com/xworkz/inheritance/internal/AI.java
+++ inheritance/src/com/xworkz/inheritance/internal/AI.java
@@ -5,4 +5,25 @@ public class AI extends Subject {
         super();
         System.out.println("Constructor in AI");
     }
+
+    @Override
+    public void study() {
+        System.out.println("Subject: Studying...in AI");
+    }
+    @Override
+    public void takeExam() {
+        System.out.println("Subject: Taking an exam...in AI");
+    }
+    @Override
+    public void submitAssignment() {
+        System.out.println("Subject: Submitting an assignment...in AI");
+    }
+    @Override
+    public void discuss() {
+        System.out.println("Subject: Discussing with peers...in AI");
+    }
+    @Override
+    public void research() {
+        System.out.println("Subject: Researching new topics...in AI");
+    }
 }
--- inheritance/src/com/xworkz/inheritance/internal/Parasyte.java
+++ inheritance/src/com/xworkz/inheritance/internal/Parasyte.java
@@ -5,4 +5,5 @@ public class Parasyte extends Anime {
         super();
         System.out.println("Constructor in Parasyte");
     }
+    
 }
