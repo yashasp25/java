e28fb9269f1b813335a59e196d0400ba4eabb7dd
yashasp25 <yashasp2k3@gmail.com>
2025-04-05 20:01:12 +0530

    Subjects override

--- inheritance/src/com/xworkz/inheritance/internal/Algorithms.java
+++ inheritance/src/com/xworkz/inheritance/internal/Algorithms.java
@@ -5,4 +5,24 @@ public class Algorithms extends Subject {
         super();
         System.out.println("Constructor in Algorithms");
     }
+    @Override
+    public void study() {
+        System.out.println("Subject: Studying...in Algorithms");
+    }
+    @Override
+    public void takeExam() {
+        System.out.println("Subject: Taking an exam...in Algorithms");
+    }
+    @Override
+    public void submitAssignment() {
+        System.out.println("Subject: Submitting an assignment...in Algorithms");
+    }
+    @Override
+    public void discuss() {
+        System.out.println("Subject: Discussing with peers...in Algorithms");
+    }
+    @Override
+    public void research() {
+        System.out.println("Subject: Researching new topics...in Algorithms");
+    }
 }
