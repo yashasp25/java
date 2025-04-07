8909565fcc028e7bb8baabb4afde12ee27b8a74f
yashasp25 <yashasp2k3@gmail.com>
2025-04-07 16:05:12 +0530

    Day-3 Anime method added

--- inheritance/src/com/xworkz/inheritance/internal/AnimeUser.java
+++ inheritance/src/com/xworkz/inheritance/internal/AnimeUser.java
@@ -8,84 +8,105 @@ public class AnimeUser {
         anime.discuss();
         anime.collectMerchandise();
 
-        if(anime instanceof OnePiece){
+        if (anime instanceof OnePiece) {
             OnePiece onePiece = (OnePiece) anime;
             onePiece.findOnePiece();
         }
-
-
-        if (anime instanceof Naruto naruto) {
+        if (anime instanceof Naruto) {
+            Naruto naruto = (Naruto) anime;
             naruto.useRasengan();
         }
-        if (anime instanceof AttackOnTitan titan) {
+        if (anime instanceof AttackOnTitan) {
+            AttackOnTitan titan = (AttackOnTitan) anime;
             titan.transformTitan();
         }
-        if (anime instanceof DeathNote note) {
+        if (anime instanceof DeathNote) {
+            DeathNote note = (DeathNote) anime;
             note.writeInDeathNote();
         }
-        if (anime instanceof DragonBallZ dbz) {
+        if (anime instanceof DragonBallZ) {
+            DragonBallZ dbz = (DragonBallZ) anime;
             dbz.kamehameha();
         }
-        if (anime instanceof DemonSlayer slayer) {
+        if (anime instanceof DemonSlayer) {
+            DemonSlayer slayer = (DemonSlayer) anime;
             slayer.useNichirinSword();
         }
-        if (anime instanceof MyHeroAcademia mha) {
+        if (anime instanceof MyHeroAcademia) {
+            MyHeroAcademia mha = (MyHeroAcademia) anime;
             mha.useOneForAll();
         }
-        if (anime instanceof TokyoRevengers revengers) {
+        if (anime instanceof TokyoRevengers) {
+            TokyoRevengers revengers = (TokyoRevengers) anime;
             revengers.timeTravel();
         }
-        if (anime instanceof Bleach bleach) {
+        if (anime instanceof Bleach) {
+            Bleach bleach = (Bleach) anime;
             bleach.useBankai();
         }
-        if (anime instanceof HunterXHunter hunter) {
+        if (anime instanceof HunterXHunter) {
+            HunterXHunter hunter = (HunterXHunter) anime;
             hunter.useNen();
         }
-        if (anime instanceof OnePunchMan opm) {
+        if (anime instanceof OnePunchMan) {
+            OnePunchMan opm = (OnePunchMan) anime;
             opm.seriousPunch();
         }
-        if (anime instanceof JujutsuKaisen jjk) {
+        if (anime instanceof JujutsuKaisen) {
+            JujutsuKaisen jjk = (JujutsuKaisen) anime;
             jjk.useCursedTechnique();
         }
-        if (anime instanceof VinlandSaga vinland) {
+        if (anime instanceof VinlandSaga) {
+            VinlandSaga vinland = (VinlandSaga) anime;
             vinland.vikingRaid();
         }
-        if (anime instanceof ChainsawMan chainsaw) {
+        if (anime instanceof ChainsawMan) {
+            ChainsawMan chainsaw = (ChainsawMan) anime;
             chainsaw.chainsawTransform();
         }
-        if (anime instanceof FairyTail fairyTail) {
+        if (anime instanceof FairyTail) {
+            FairyTail fairyTail = (FairyTail) anime;
             fairyTail.useFireDragonMagic();
         }
-        if (anime instanceof SwordArtOnline sao) {
+        if (anime instanceof SwordArtOnline) {
+            SwordArtOnline sao = (SwordArtOnline) anime;
             sao.equipLegendarySword();
         }
-        if (anime instanceof CodeGeass geass) {
+        if (anime instanceof CodeGeass) {
+            CodeGeass geass = (CodeGeass) anime;
             geass.activateGeass();
         }
-        if (anime instanceof FullmetalAlchemist fma) {
+        if (anime instanceof FullmetalAlchemist) {
+            FullmetalAlchemist fma = (FullmetalAlchemist) anime;
             fma.humanTransmutation();
         }
-        if (anime instanceof BlackClover clover) {
+        if (anime instanceof BlackClover) {
+            BlackClover clover = (BlackClover) anime;
             clover.summonMagicBook();
         }
-        if (anime instanceof SteinsGate gate) {
+        if (anime instanceof SteinsGate) {
+            SteinsGate gate = (SteinsGate) anime;
             gate.hackTimeLine();
         }
-        if (anime instanceof Parasyte parasyte) {
+        if (anime instanceof Parasyte) {
+            Parasyte parasyte = (Parasyte) anime;
             parasyte.useParasiteArm();
         }
-        if (anime instanceof CowboyBebop bebop) {
+        if (anime instanceof CowboyBebop) {
+            CowboyBebop bebop = (CowboyBebop) anime;
             bebop.collectBounty();
         }
-        if (anime instanceof NeonGenesisEvangelion eva) {
+        if (anime instanceof NeonGenesisEvangelion) {
+            NeonGenesisEvangelion eva = (NeonGenesisEvangelion) anime;
             eva.pilotEvaUnit();
         }
-        if (anime instanceof ReZero rezero) {
+        if (anime instanceof ReZero) {
+            ReZero rezero = (ReZero) anime;
             rezero.returnByDeath();
         }
-        if (anime instanceof MobPsycho100 mob) {
+        if (anime instanceof MobPsycho100) {
+            MobPsycho100 mob = (MobPsycho100) anime;
             mob.usePsychicPowers();
         }
-
     }
 }
