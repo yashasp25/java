8fefeaf6ad7bba6279841eb59ccb73af69185512
yashasp25 <yashasp2k3@gmail.com>
2025-04-16 00:16:38 +0530

    abstract

---
+++ abstract/src/com/xworkz/internal/Manager.java
@@ -0,0 +1,19 @@
+package com.xworkz.internal;
+
+class Manager extends Employee {
+    Manager(String name) {
+        super(name);
+    }
+
+    Manager(String name, String department) {
+        super(name, department);
+    }
+
+    Manager(String name, String department, double salary, int experience) {
+        super(name, department, salary, experience);
+    }
+
+    void details() {
+        System.out.println("Manager: " + name + ", Dept: " + department + ", Salary: " + salary + ", Exp: " + experience + " yrs");
+    }
+}
