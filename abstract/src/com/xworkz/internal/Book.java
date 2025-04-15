a656233c05a30db1fc3e263cdd929b0945e55b6e
yashasp25 <yashasp2k3@gmail.com>
2025-04-16 00:13:34 +0530

    abstract

---
+++ abstract/src/com/xworkz/internal/Book.java
@@ -0,0 +1,13 @@
+package com.xworkz.internal;
+
+abstract class Book {
+    String title;
+    String author;
+    int year;
+
+    Book(String title, String author, int year) {
+        this.title = title;
+        this.author = author;
+        this.year = year;
+    }
+}
