07530232933518f8b6e1376941c65c673086a309
yashasp25 <yashasp2k3@gmail.com>
2025-04-22 23:08:35 +0530

    interface 47

---
+++ interfacecomplete/src/com/xworkz/interfacelast/implement/EditorImpl.java
@@ -0,0 +1,10 @@
+package com.xworkz.interfacelast.implement;
+
+import com.xworkz.interfacelast.interfacecs.Editor;
+
+public class EditorImpl implements Editor {
+    @Override
+    public void edit() {
+        System.out.println("run edit in implements");
+    }
+}
---
+++ interfacecomplete/src/com/xworkz/interfacelast/implement/EmployeeImpl.java
@@ -0,0 +1,11 @@
+package com.xworkz.interfacelast.implement;
+
+import com.xworkz.interfacelast.interfacecs.Employee;
+
+public class EmployeeImpl implements Employee {
+    @Override
+    public void work() {
+        System.out.println("run work in implementation...");
+
+    }
+}
