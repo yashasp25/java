9d50fc4e40971e655921aeb6b3e338cfa31b9ae9
yashasp25 <yashasp2k3@gmail.com>
2025-04-22 23:07:33 +0530

    interface 47

---
+++ interfacecomplete/src/com/xworkz/interfacelast/implement/DirectorImpl.java
@@ -0,0 +1,10 @@
+package com.xworkz.interfacelast.implement;
+
+import com.xworkz.interfacelast.interfacecs.Director;
+
+public class DirectorImpl implements Director {
+    @Override
+    public void direct() {
+        System.out.println("run direct in implements");
+    }
+}
---
+++ interfacecomplete/src/com/xworkz/interfacelast/implement/DoctorImpl.java
@@ -0,0 +1,11 @@
+package com.xworkz.interfacelast.implement;
+
+import com.xworkz.interfacelast.interfacecs.Doctor;
+
+public class DoctorImpl implements Doctor {
+    @Override
+    public void treatPatient() {
+        System.out.println("run treat patient in implementation...");
+
+    }
+}
