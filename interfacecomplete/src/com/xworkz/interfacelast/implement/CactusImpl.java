d6670aa317156b19bdac9da2d72cefc11a24e318
yashasp25 <yashasp2k3@gmail.com>
2025-04-22 23:03:46 +0530

    interface 47

---
+++ interfacecomplete/src/com/xworkz/interfacelast/implement/CactusImpl.java
@@ -0,0 +1,10 @@
+package com.xworkz.interfacelast.implement;
+
+import com.xworkz.interfacelast.interfacecs.Cactus;
+
+public class CactusImpl implements Cactus {
+    @Override
+    public void storeWater() {
+        System.out.println("run store water in implements");
+    }
+}
