1248310cd14cb81fcd53cae3b6d71ee6729200ea
yashasp25 <yashasp2k3@gmail.com>
2025-04-22 10:06:53 +0530

    connector

--- connect/src/com/xworkz/connector/internal/Connectorimpl.java
+++ connect/src/com/xworkz/connector/internal/Connectorimpl.java
@@ -3,7 +3,7 @@ package com.xworkz.connector.internal;
 public class Connectorimpl implements Connector{
 
     @Override
-    public void execute() {
+    public void run() {
         System.out.println("Run execute in Connectorimpl");
     }
 }
