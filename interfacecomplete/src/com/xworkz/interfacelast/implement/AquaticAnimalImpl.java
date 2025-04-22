cdc4c31ae23ea896ddc89dd9f81d2b3b223a68af
yashasp25 <yashasp2k3@gmail.com>
2025-04-22 23:02:10 +0530

    interface 47

---
+++ interfacecomplete/src/com/xworkz/interfacelast/implement/AquaticAnimalImpl.java
@@ -0,0 +1,10 @@
+package com.xworkz.interfacelast.implement;
+
+import com.xworkz.interfacelast.interfacecs.AquaticAnimal;
+
+public class AquaticAnimalImpl implements AquaticAnimal {
+    @Override
+    public void breath() {
+        System.out.println("run breath in implements");
+    }
+}
