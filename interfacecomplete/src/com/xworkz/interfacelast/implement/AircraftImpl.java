f08071bf229bd2ab01b3c114a3595573a9c51b30
yashasp25 <yashasp2k3@gmail.com>
2025-04-22 22:37:08 +0530

    interface 47

---
+++ interfacecomplete/src/com/xworkz/interfacelast/implement/AircraftImpl.java
@@ -0,0 +1,10 @@
+package com.xworkz.interfacelast.implement;
+
+import com.xworkz.interfacelast.interfacecs.Aircraft;
+
+public class AircraftImpl implements Aircraft {
+    @Override
+    public void fly() {
+        System.out.println("run fly in implements");
+    }
+}
