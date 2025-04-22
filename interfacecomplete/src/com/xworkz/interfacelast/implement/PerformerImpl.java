3e0c19d23ed40c86ed911ab0ea2586cc779b7c3f
yashasp25 <yashasp2k3@gmail.com>
2025-04-22 23:42:20 +0530

    interface 47

---
+++ interfacecomplete/src/com/xworkz/interfacelast/implement/PerformerImpl.java
@@ -0,0 +1,10 @@
+package com.xworkz.interfacelast.implement;
+
+import com.xworkz.interfacelast.interfacecs.Performer;
+
+public class PerformerImpl implements Performer {
+    @Override
+    public void perform() {
+        System.out.println("run perform in implements");
+    }
+}
